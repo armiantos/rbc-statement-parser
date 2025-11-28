290103248c608818c188ac9b86465801341f19f1
Armianto Sumitro <armiantos@gmail.com>
2025-11-27 18:31:56 -0700

    Simplify schema

--- app/dedup.py
+++ app/dedup.py
@@ -9,7 +9,7 @@ def get_connection() -> sqlite3.Connection:
 def upsert_schema():
     con = get_connection()
     cursor = con.cursor()
-    cursor.execute('create table if not exists scanned (id integer primary key autoincrement, file nvarchar(256), hash char(20))')
+    cursor.execute('create table if not exists scanned (id integer primary key autoincrement, hash char(20))')
 
 def has_been_parsed(
     path_to_file: str
@@ -18,10 +18,8 @@ def has_been_parsed(
         contents = file.read()
         sha1_hash = hashlib.sha1(contents).hexdigest()
         con = sqlite3.connect(path_to_scanned)
-        res = con.execute('select hash from scanned where file = ?', [path_to_file]).fetchone()
-        if res is None:
-            return False
-        return res[0] == sha1_hash
+        res = con.execute('select hash from scanned where hash = ?', [sha1_hash]).fetchone()
+        return res != None
 
 def mark_as_parsed(
     path_to_file: str
@@ -30,5 +28,5 @@ def mark_as_parsed(
         contents = file.read()
         sha1_hash = hashlib.sha1(contents).hexdigest()
         con = sqlite3.connect(path_to_scanned)
-        con.execute('insert into scanned(file, hash) values (?, ?)', (path_to_file, sha1_hash)).fetchone()
+        con.execute('insert into scanned(hash) values (?)', [sha1_hash]).fetchone()
         con.commit()
