02323aa6040dbb43aa81bb6bf0ad8c488bdc0165
Armianto Sumitro <armiantos@gmail.com>
2025-11-27 18:12:01 -0700

    Add check whether file has been parsed

---
+++ app/dedup.py
@@ -0,0 +1,25 @@
+import hashlib
+import sqlite3
+
+path_to_scanned = './rbc-statement-parser.db'
+
+def get_connection() -> sqlite3.Connection:
+    return sqlite3.connect(path_to_scanned)
+
+def upsert_schema():
+    con = get_connection()
+    cursor = con.cursor()
+    cursor.execute('create table if not exists scanned (id integer primary key autoincrement, file nvarchar(256), hash char(20))')
+
+def has_been_parsed(
+    path_to_file: str
+) -> bool:
+    upsert_schema()
+    with open(path_to_file, mode='rb') as file:
+        contents = file.read()
+        sha1_hash = hashlib.sha1(contents)
+        con = sqlite3.connect(path_to_scanned)
+        res = con.execute('select hash from scanned where file = ?', [path_to_file]).fetchone()
+        if res is None:
+            return False
+        return res[0] == sha1_hash
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 import sys
 
 from app.chequing import is_chequing, parse_chequing
+from app.dedup import has_been_parsed
 from app.entities import Config
 from app.utils import format_transaction, write_file
 from app.visa import is_visa, parse_visa
@@ -25,6 +26,7 @@ def parse_files(path: str) -> list:
             os.path.abspath(os.path.join(path, f))
             for f in os.listdir(path)
             if f.lower().endswith(".pdf")
+                and not has_been_parsed(os.path.join(path, f))
         ]
     elif os.path.isfile(path) and path.lower().endswith(".pdf"):
         files = [os.path.abspath(path)]
